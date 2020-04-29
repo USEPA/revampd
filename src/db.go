4403b31f729bda29f01b9c1b086d5db65dcc6a63
maheese <heese.michael@epa.gov>
2020-04-29 10:16:46 -0400

    Limited the query to 100 results

--- src/db.go
+++ src/db.go
@@ -47,7 +47,7 @@ func CreateDatabaseService() (*DatabaseService, error) {
 //FindUnitsByOperatingYear returns the units for the given year
 func (dbService *DatabaseService) FindUnitsByOperatingYear(year int) ([]Unit, error) {
 	log.Debug("Finding units for year ", year)
-	rows, err := dbService.database.Queryx("SELECT unitid, op_year, facility_name, oris_code, state, epa_region, unit_type_description, stack_ids, op_status, program_code, primary_fuel_type_desc, primary_fuel_group, an_count_op_time, an_gload, an_sload, an_heat_input, an_co2_mass, an_so2_mass, an_nox_mass FROM UNIT_UNIVERSE where op_year = $1", year)
+	rows, err := dbService.database.Queryx("SELECT unitid, op_year, facility_name, oris_code, state, epa_region, unit_type_description, stack_ids, op_status, program_code, primary_fuel_type_desc, primary_fuel_group, an_count_op_time, an_gload, an_sload, an_heat_input, an_co2_mass, an_so2_mass, an_nox_mass FROM UNIT_UNIVERSE where op_year = $1 limit 100", year)
 
 	units := []Unit{}
 		
