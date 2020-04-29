1ab6c3a41421e5880a9b3477d58fe18699766df6
Theresa Nguyen <nguyen.theresa@epa.gov>
2020-04-29 12:50:25 -0400

    Update unit.go

--- src/unit.go
+++ src/unit.go
@@ -2,24 +2,24 @@ package main
 
 //The Unit entity
 type Unit struct {
-	OperationYear *int `db:"op_year"`
-	UnitID *string `db:"unitid"` 
+	operatingYear *int `db:"op_year"`
+	unitID *string `db:"unitid"` 
 	EPARegion *int `db:"epa_region"` 
-	State *string `db:"state"` 
-	FacilityName *string `db:"facility_name"` 
-	ORISCode *int `db:"oris_code"` 
-	StackIDs *string `db:"stack_ids"`
-	OperationStatus	*string `db:"op_status"` 
-	ProgramCode *string `db:"program_code"` 
-	UnitType *string `db:"unit_type_description"` 
-	PrimaryFuel *string `db:"primary_fuel_type_desc"`
-	PrimaryFuelGroup *string `db:"primary_fuel_group"` 
-	OperationTime *int64 `db:"an_count_op_time"` 
-	GrossLoad *int64 `db:"an_gload"` 
-	SteamLoad *int64 `db:"an_sload"` 
-	HeatInput *float64 `db:"an_heat_input"` 
-	CO2Mass *float64 `db:"an_co2_mass"`
-	SO2Mass *float64 `db:"an_so2_mass"` 
-	NOXMass *float64 `db:"an_nox_mass"`
+	EPAState *string `db:"state"` 
+	facilityName *string `db:"facility_name"` 
+	orisCode *int `db:"oris_code"` 
+	stackId *string `db:"stack_ids"`
+	operatingStatus	*string `db:"op_status"` 
+	programCode *string `db:"program_code"` 
+	unitType *string `db:"unit_type_description"` 
+	primaryFuel *string `db:"primary_fuel_type_desc"`
+	primaryFuelGroup *string `db:"primary_fuel_group"` 
+	operatingTime *int64 `db:"an_count_op_time"` 
+	grossLoad *int64 `db:"an_gload"` 
+	steamLoad *int64 `db:"an_sload"` 
+	heatInput *float64 `db:"an_heat_input"` 
+	co2Mass *float64 `db:"an_co2_mass"`
+	so2Mass *float64 `db:"an_so2_mass"` 
+	noxMass *float64 `db:"an_nox_mass"`
 }
 
