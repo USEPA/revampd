a7dc933a5a0554f0986ff1adde269c0816de6739
Joseph Thompson <thompson.joseph@epa.gov>
2020-05-15 15:28:28 -0400

    added env var for dev nginx

---
+++ html/src/app.js
@@ -0,0 +1,25 @@
+//var apiUrl = <!--# echo var="API_URL" -->;
+function buildUrl (url) {
+    return apiUrl + url + "?operatingYear=2018"
+  }
+  
+  const vm = new Vue({
+    el: '#app',
+    data: {
+    vueCheck: 45,
+    results: []
+    },
+    mounted () {
+      this.getPosts("/units/findByOperatingYear");
+    },
+    methods: {
+      getPosts(section) {
+        let url = apiUrl+section+"?operatingYear=2018";
+        console.log(url);
+        axios.get(url).then(response => {
+        console.log(response.data);
+          this.results = response.data;
+        }).catch( error => { console.log(error); });
+      }
+    }
+  });
